41c2410a8b3c47088e0bd992de1fea3d3c26c1dc
rahulmhjn <rahul.mahajan676@gmail.com>
2020-07-30 19:05:01 +0530

    setting search bar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-class App extends React.Component {
-  render() {
-    return <div>Hello!</div>;
-  }
-}
+import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
